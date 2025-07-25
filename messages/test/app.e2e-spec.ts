66b6077b7d3ab5a8926c6e05b9a3ebd6db74b794
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2025-07-25 09:48:05 +0530

    chore: update dependencies and remove unused test file
    - Updated NestJS dependencies to version 11.1.5
    - Updated rxjs to version 7.8.2
    - Updated devDependencies including eslint, jest, prettier, and typescript
    - Removed unused app.controller.spec.ts test file
    - Updated e2e test to use MessagesModule and changed endpoint to /messages

--- messages/test/app.e2e-spec.ts
+++ messages/test/app.e2e-spec.ts
@@ -2,24 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { MessagesModule } from './../src/messages/messages.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [MessagesModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/messages (GET)', () => {
+    return request(app.getHttpServer()).get('/messages').expect(404); // MessagesController doesn't have any routes defined yet
   });
 });
