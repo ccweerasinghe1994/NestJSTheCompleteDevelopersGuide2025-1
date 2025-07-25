ae6e590c6d90c3fe56868c72d01c639635adfcf7
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2025-07-25 09:51:27 +0530

    fix: correct formatting in main.ts and messages.module.ts

--- messages/src/main.ts
+++ messages/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
   await app.listen(process.env.PORT ?? 3000);
